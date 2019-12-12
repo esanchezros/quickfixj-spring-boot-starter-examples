76f824ab895e198a442487cc50e2df64a19b20fc
Eduardo Sanchez-Ros <esanchezros@yahoo.es>
2019-12-12 14:27:27 +0000

    Examples enhancements (#6)
    * Added some more features to examples, wip
    
    * Simplified module names
    
    * Simplified module structure
    
    * Added simple sender example using quickfixj template
    
    * Improved example by making message to send configurable, still work in progress
    
    * Fixed docker examples and artifact naming
    
    * Fixed indentation and refreshed copyright notice
    
    * Minor changes
    
    * Renamed module and enhanced sender controller
    
    * Updates poms
    
    * Updated version of quickfixj starter

---
+++ simple-client/src/main/java/io/allune/quickfixj/spring/boot/starter/examples/client/AppClient.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.allune.quickfixj.spring.boot.starter.examples.client;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.allune.quickfixj.spring.boot.starter.EnableQuickFixJClient;
+import lombok.extern.slf4j.Slf4j;
+import quickfix.Application;
+import quickfix.ConfigError;
+import quickfix.FileLogFactory;
+import quickfix.Initiator;
+import quickfix.LogFactory;
+import quickfix.MessageFactory;
+import quickfix.MessageStoreFactory;
+import quickfix.SessionSettings;
+import quickfix.ThreadedSocketInitiator;
+
+@Slf4j
+@EnableQuickFixJClient
+@SpringBootApplication
+public class AppClient implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppClient.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Joining thread, you can press Ctrl+C to shutdown application");
+		Thread.currentThread().join();
+	}
+
+	@Bean
+	public Application clientApplication() {
+		return new ClientApplicationAdapter();
+	}
+
+	@Bean
+	public Initiator clientInitiator(quickfix.Application clientApplication, MessageStoreFactory clientMessageStoreFactory,
+			SessionSettings clientSessionSettings, LogFactory clientLogFactory,
+			MessageFactory clientMessageFactory) throws ConfigError {
+
+		return new ThreadedSocketInitiator(clientApplication, clientMessageStoreFactory, clientSessionSettings,
+				clientLogFactory, clientMessageFactory);
+	}
+
+	@Bean
+	public LogFactory clientLogFactory(SessionSettings clientSessionSettings) {
+		return new FileLogFactory(clientSessionSettings);
+	}
+}
---
+++ simple-server-listener/src/main/java/io/allune/quickfixj/spring/boot/starter/examples/server/ApplicationListener.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.allune.quickfixj.spring.boot.starter.examples.server;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import io.allune.quickfixj.spring.boot.starter.model.Create;
+import io.allune.quickfixj.spring.boot.starter.model.FromAdmin;
+import io.allune.quickfixj.spring.boot.starter.model.FromApp;
+import io.allune.quickfixj.spring.boot.starter.model.Logon;
+import io.allune.quickfixj.spring.boot.starter.model.Logout;
+import io.allune.quickfixj.spring.boot.starter.model.ToAdmin;
+import io.allune.quickfixj.spring.boot.starter.model.ToApp;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class ApplicationListener {
+
+	@EventListener
+	public void handleFromAdmin1(FromAdmin fromAdmin) {
+		log.info("fromAdmin: Message={}, SessionId={}", fromAdmin.getMessage(), fromAdmin.getSessionId());
+	}
+
+	@EventListener
+	public void handleFromAdmin2(FromAdmin fromAdmin) {
+		log.info("fromAdmin: Message={}, SessionId={}", fromAdmin.getMessage(), fromAdmin.getSessionId());
+	}
+
+	@EventListener
+	public void handleFromApp(FromApp fromApp) {
+		log.info("fromAdmin: Message={}, SessionId={}", fromApp.getMessage(), fromApp.getSessionId());
+	}
+
+	@EventListener
+	public void handleCreate(Create create) {
+		log.info("onCreate: SessionId={}", create.getSessionId());
+	}
+
+	@EventListener
+	public void handleLogon(Logon logon) {
+		log.info("onLogon: SessionId={}", logon.getSessionId());
+	}
+
+	@EventListener
+	public void handleLogout(Logout logout) {
+		log.info("onLogout: SessionId={}", logout.getSessionId());
+	}
+
+	@EventListener
+	public void handleToAdmin(ToAdmin toAdmin) {
+		log.info("toAdmin: SessionId={}", toAdmin.getSessionId());
+	}
+
+	@EventListener
+	public void handleToApp(ToApp toApp) {
+		log.info("toApp: SessionId={}", toApp.getSessionId());
+	}
+}
