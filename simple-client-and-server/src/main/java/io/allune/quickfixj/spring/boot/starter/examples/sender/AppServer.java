16b2e0240ecd56f1c433d71209a3831e5eddc3eb
Eduardo Sanchez-Ros <esanchezros@yahoo.es>
2020-07-08 13:21:27 +0200

    Updated starter version, documentation and comments (#11)

--- simple-client-and-server/src/main/java/io/allune/quickfixj/spring/boot/starter/examples/sender/AppServer.java
+++ simple-client-and-server/src/main/java/io/allune/quickfixj/spring/boot/starter/examples/sender/AppServer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2017-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,14 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.allune.quickfixj.spring.boot.starter.examples.sender;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.allune.quickfixj.spring.boot.starter.EnableQuickFixJClient;
 import io.allune.quickfixj.spring.boot.starter.EnableQuickFixJServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableQuickFixJServer
 @EnableQuickFixJClient
--- simple-client/src/main/java/io/allune/quickfixj/spring/boot/starter/examples/client/AppClient.java
+++ simple-client/src/main/java/io/allune/quickfixj/spring/boot/starter/examples/client/AppClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2017-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,16 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.allune.quickfixj.spring.boot.starter.examples.client;
 
+import io.allune.quickfixj.spring.boot.starter.EnableQuickFixJClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import io.allune.quickfixj.spring.boot.starter.EnableQuickFixJClient;
-import lombok.extern.slf4j.Slf4j;
 import quickfix.Application;
 import quickfix.ConfigError;
 import quickfix.FileLogFactory;
