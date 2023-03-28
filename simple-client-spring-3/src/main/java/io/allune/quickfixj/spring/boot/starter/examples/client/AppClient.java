c3b5e008caa3718709bacfbb979c8dbfb6435be6
Eduardo Sanchez-Ros <esanchezros@yahoo.es>
2023-03-28 14:12:14 +0200

    Updated spring boot starter to 2.15.3

---
+++ simple-client-spring-3/src/main/java/io/allune/quickfixj/spring/boot/starter/examples/client/AppClient.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.allune.quickfixj.spring.boot.starter.examples.client;
+
+import io.allune.quickfixj.spring.boot.starter.EnableQuickFixJClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import quickfix.Application;
+import quickfix.FileLogFactory;
+import quickfix.LogFactory;
+import quickfix.SessionSettings;
+import quickfix.fix41.MessageCracker;
+
+@Slf4j
+@EnableQuickFixJClient
+@SpringBootApplication
+public class AppClient {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppClient.class, args);
+	}
+
+	@Bean
+	public Application clientApplication(MessageCracker messageCracker) {
+		return new ClientApplicationAdapter(messageCracker);
+	}
+
+	@Bean
+	public MessageCracker messageCracker() {
+		return new ApplicationMessageCracker();
+	}
+
+//	@Bean
+//	public Initiator clientInitiator(
+//			Application clientApplication,
+//			MessageStoreFactory clientMessageStoreFactory,
+//			SessionSettings clientSessionSettings,
+//			LogFactory clientLogFactory,
+//			MessageFactory clientMessageFactory) throws ConfigError {
+//
+//		return new ThreadedSocketInitiator(clientApplication, clientMessageStoreFactory, clientSessionSettings,
+//				clientLogFactory, clientMessageFactory);
+//	}
+
+	@Bean
+	public LogFactory clientLogFactory(SessionSettings clientSessionSettings) {
+		return new FileLogFactory(clientSessionSettings);
+	}
+}
---
+++ simple-client-spring-3/src/main/java/io/allune/quickfixj/spring/boot/starter/examples/client/ClientApplicationAdapter.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2017-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.allune.quickfixj.spring.boot.starter.examples.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import quickfix.Application;
+import quickfix.FieldNotFound;
+import quickfix.IncorrectTagValue;
+import quickfix.Message;
+import quickfix.SessionID;
+import quickfix.UnsupportedMessageType;
+import quickfix.fix41.MessageCracker;
+
+public class ClientApplicationAdapter implements Application {
+
+	private static final Logger log = LoggerFactory.getLogger(ClientApplicationAdapter.class);
+
+	private final MessageCracker messageCracker;
+
+	public ClientApplicationAdapter(MessageCracker messageCracker) {
+		this.messageCracker = messageCracker;
+	}
+
+	@Override
+	public void fromAdmin(Message message, SessionID sessionId) {
+		log.info("fromAdmin: Message={}, SessionId={}", message, sessionId);
+	}
+
+	@Override
+	public void fromApp(Message message, SessionID sessionId) {
+		log.info("fromApp: Message={}, SessionId={}", message, sessionId);
+
+		try {
+			messageCracker.crack(message, sessionId);
+		} catch (UnsupportedMessageType | FieldNotFound | IncorrectTagValue e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public void onCreate(SessionID sessionId) {
+		log.info("onCreate: SessionId={}", sessionId);
+	}
+
+	@Override
+	public void onLogon(SessionID sessionId) {
+		log.info("onLogon: SessionId={}", sessionId);
+	}
+
+	@Override
+	public void onLogout(SessionID sessionId) {
+		log.info("onLogout: SessionId={}", sessionId);
+	}
+
+	@Override
+	public void toAdmin(Message message, SessionID sessionId) {
+		log.info("toAdmin: Message={}, SessionId={}", message, sessionId);
+	}
+
+	@Override
+	public void toApp(Message message, SessionID sessionId) {
+		log.info("toApp: Message={}, SessionId={}", message, sessionId);
+	}
+}
